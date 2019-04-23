5802a428e45dc17ef779b4776c613615ee97306f
verhalenn <verhalenn@yahoo.com>
2019-04-23 16:30:24 -0500

    Added layout to Step Size Information Tab

--- dashstan/diagnostics/step_size_information.py
+++ dashstan/diagnostics/step_size_information.py
@@ -14,23 +14,37 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>
 import dash_core_components as dcc
+import dash_html_components as html
 from dashstan import dashstantab
 import plotly.graph_objs as go
+
+STANDARD_HEIGHT = 300
+STANDARD_MARGIN = dict(l=50, r=20, t=50, b=50, pad=5)
 
 
 class StepSizeInformation(dashstantab.DashStanTab):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.label='Step Size Information'
+        self.label = 'Step Size Information'
         self.data_warmup = self.data[self.data['warmup'] == 0]
         self._build_children()
 
     def _build_children(self):
         self.children = [
-            self._build_step(),
-            self._build_step_log_voilin(),
-            self._build_step_metro_violin(),
+            html.Div(className='container-fluid', children=[
+                # html.Div(className='row col', children=[
+                    self._build_step(),
+                # ]),
+                html.Div(className='row', children=[
+                    html.Div(className='col', children=[
+                        self._build_step_metro_violin(),
+                    ]),
+                    html.Div(className='col', children=[
+                        self._build_step_log_voilin(),
+                    ]),
+                ]),
+            ]),
         ]
 
     def _build_step(self):
@@ -43,10 +57,13 @@ class StepSizeInformation(dashstantab.DashStanTab):
         ]
 
         layout = go.Layout(
-            title='Sampled Step Size'
+            title='Sampled Step Size',
+            legend=dict(orientation='h', y=0),
+            margin=STANDARD_MARGIN,
         )
 
         return dcc.Graph(id='step-graph',
+                         style=dict(height=STANDARD_HEIGHT),
                          figure={
                              'data': traces,
                              'layout': layout,
@@ -55,16 +72,22 @@ class StepSizeInformation(dashstantab.DashStanTab):
     def _build_step_log_voilin(self):
         traces = [{
             'type': 'violin',
-            'x': self.get_data_warmup()['stepsize__'],
+            'x': self.get_data_warmup()['stepsize__'].round(4),
             'y': self.get_data_warmup()['lp__']
         }]
         layout = go.Layout(
+            margin=STANDARD_MARGIN,
             xaxis={
                 'type': 'category',
-            }
+                'title': 'Step Size'
+            },
+            yaxis={
+                'title': 'Log Probability'
+            },
         )
 
         return dcc.Graph(id='step-lp',
+                         style=dict(height=STANDARD_HEIGHT),
                          figure={
                              'data': traces,
                              'layout': layout,
@@ -74,16 +97,22 @@ class StepSizeInformation(dashstantab.DashStanTab):
     def _build_step_metro_violin(self):
         traces = [{
             'type': 'violin',
-            'x': self.get_data_warmup()['stepsize__'],
+            'x': self.get_data_warmup()['stepsize__'].round(4),
             'y': self.get_data_warmup()['accept_stat__']
         }]
         layout = go.Layout(
             xaxis={
                 'type': 'category',
-            }
+                'title': 'Step Size',
+            },
+            yaxis={
+                'title': 'Mean Metrop. Acceptance',
+            },
+            margin=STANDARD_MARGIN,
         )
 
         return dcc.Graph(id='step-metrop',
+                         style=dict(height=STANDARD_HEIGHT),
                          figure={
                              'data': traces,
                              'layout': layout,
